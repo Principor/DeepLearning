8150dfd06446d52a8b59259c2dde406d0341c3bd
Christopher Jewszel <c.jewszel@gmail.com>
2023-04-14 23:30:14 +0100

    Changed formatting and increased scope of tests

--- DeepLearningTest/DeepLearningTest.cpp
+++ DeepLearningTest/DeepLearningTest.cpp
@@ -4,13 +4,12 @@
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace DeepLearningTest
+namespace TensorTest
 {
-	TEST_CLASS(DeepLearningTest)
+	TEST_CLASS(ZeroesTest)
 	{
 	public:
-
-		TEST_METHOD(TestZeroesShapes)
+		TEST_METHOD(Shape)
 		{
 			Assert::AreEqual(1, Tensor::zeroes({ 1 }).getShape()[0]);
 
@@ -24,7 +23,7 @@ namespace DeepLearningTest
 			Assert::AreEqual(5, Tensor::zeroes({ 3,7,5 }).getShape()[2]);
 		}
 
-		TEST_METHOD(TestZeroesSizes)
+		TEST_METHOD(Size)
 		{
 			Assert::AreEqual(1, Tensor::zeroes({}).getSize());
 			Assert::AreEqual(1, Tensor::zeroes({ 1 }).getSize());
@@ -34,7 +33,7 @@ namespace DeepLearningTest
 			Assert::AreEqual(105, Tensor::zeroes({ 3, 7, 5 }).getSize());
 		}
 
-		TEST_METHOD(TestZeroesValues)
+		TEST_METHOD(Values)
 		{
 			Tensor tensor1 = Tensor::zeroes({ 3 });
 			Assert::IsTrue(std::abs(tensor1.get({ 0 }).item()) < 1e-10);
@@ -50,28 +49,102 @@ namespace DeepLearningTest
 			Assert::IsTrue(std::abs(tensor2.get({ 1, 2 }).item()) < 1e-10);
 		}
 
-		TEST_METHOD(TestInvalidShapes)
+		TEST_METHOD(InvalidShape)
 		{
 			Assert::ExpectException<std::invalid_argument>([]() { Tensor::zeroes({ -1 }); });
 			Assert::ExpectException<std::invalid_argument>([]() { Tensor::zeroes({ 0 }); });
 			Assert::ExpectException<std::invalid_argument>([]() { Tensor::zeroes({ 1, 0, 3 }); });
 		}
+	};
 
 
-		TEST_METHOD(TestItemInvalidShapes)
+	TEST_CLASS(FullTest)
+	{
+	public:
+		TEST_METHOD(Shape)
+		{
+			Assert::AreEqual(1, Tensor::full({ 1 }, 0.0f).getShape()[0]);
+
+			Assert::AreEqual(3, Tensor::full({ 3 }, 1.0f).getShape()[0]);
+
+			Assert::AreEqual(5, Tensor::full({ 5, 2 }, -2.0f).getShape()[0]);
+			Assert::AreEqual(2, Tensor::full({ 5, 2 }, -2.0f).getShape()[1]);
+
+			Assert::AreEqual(3, Tensor::full({ 3,7,5 }, 1.0f).getShape()[0]);
+			Assert::AreEqual(7, Tensor::full({ 3,7,5 }, 1.0f).getShape()[1]);
+			Assert::AreEqual(5, Tensor::full({ 3,7,5 }, 1.0f).getShape()[2]);
+		}
+
+		TEST_METHOD(Size)
+		{
+			Assert::AreEqual(1, Tensor::full({}, -1.0f).getSize());
+			Assert::AreEqual(1, Tensor::full({ 1 }, 2.0f).getSize());
+			Assert::AreEqual(1, Tensor::full({ 1, 1, 1 }, 0.0f).getSize());
+			Assert::AreEqual(2, Tensor::full({ 2 }, 1.0f).getSize());
+			Assert::AreEqual(9, Tensor::full({ 3, 3 }, 5.0f).getSize());
+			Assert::AreEqual(105, Tensor::full({ 3, 7, 5 }, 1.0f).getSize());
+		}
+
+		TEST_METHOD(Values)
+		{
+			Tensor tensor1 = Tensor::full({ 3 }, 4.0f);
+			Assert::IsTrue(std::abs(tensor1.get({ 0 }).item() - 4.0f) < 1e-10);
+			Assert::IsTrue(std::abs(tensor1.get({ 1 }).item() - 4.0f) < 1e-10);
+			Assert::IsTrue(std::abs(tensor1.get({ 2 }).item() - 4.0f) < 1e-10);
+
+			Tensor tensor2 = Tensor::full({ 2, 3 }, 2.0f);
+			Assert::IsTrue(std::abs(tensor2.get({ 0, 0 }).item() - 2.0f) < 1e-10);
+			Assert::IsTrue(std::abs(tensor2.get({ 0, 1 }).item() - 2.0f) < 1e-10);
+			Assert::IsTrue(std::abs(tensor2.get({ 0, 2 }).item() - 2.0f) < 1e-10);
+			Assert::IsTrue(std::abs(tensor2.get({ 1, 0 }).item() - 2.0f) < 1e-10);
+			Assert::IsTrue(std::abs(tensor2.get({ 1, 1 }).item() - 2.0f) < 1e-10);
+			Assert::IsTrue(std::abs(tensor2.get({ 1, 2 }).item() - 2.0f) < 1e-10);
+		}
+
+		TEST_METHOD(InvalidShape)
+		{
+			Assert::ExpectException<std::invalid_argument>([]() { Tensor::full({ -1 }, 0.0f); });
+			Assert::ExpectException<std::invalid_argument>([]() { Tensor::full({ 0 }, 2.0f); });
+			Assert::ExpectException<std::invalid_argument>([]() { Tensor::full({ 1, 0, 3 }, 10.0f); });
+		}
+	};
+
+	TEST_CLASS(ItemTest)
+	{
+	public:
+		TEST_METHOD(InvalidShape)
 		{
 			Assert::ExpectException<std::length_error>([]() {Tensor::zeroes({ 3 }).item(); });
 			Assert::ExpectException<std::length_error>([]() {Tensor::zeroes({ 1, 1, 2 }).item(); });
 			Assert::ExpectException<std::length_error>([]() {Tensor::zeroes({ 1, 4, 1, 1 }).item(); });
 		}
 
-		TEST_METHOD(TestInvalidReshapes) {
+		TEST_METHOD(Value) {
+			Assert::IsTrue(std::abs(Tensor::zeroes({ }).item()) < 1e-10);
+			Assert::IsTrue(std::abs(Tensor::zeroes({ 1, 1, 1 }).item()) < 1e-10);
+
+			Tensor tensor = Tensor::zeroes({ 2,3 });
+			tensor.set({ 1 }, -1.0f);
+			Assert::IsTrue(std::abs(tensor.get({ 1,2 }).item() + 1.0f) < 1e-10);
+		}
+	};
+
+	TEST_CLASS(ReshapeTest)
+	{
+		TEST_METHOD(InvalidShape)
+		{
+			Assert::ExpectException<std::invalid_argument>([]() { Tensor::zeroes({ 1 }).reshape({ -1 }); });
+			Assert::ExpectException<std::invalid_argument>([]() { Tensor::zeroes({ 1 }).reshape({ 0 }); });
+			Assert::ExpectException<std::invalid_argument>([]() { Tensor::zeroes({ 1 }).reshape({ 1, 0, 3 }); });
+		}
+
+		TEST_METHOD(WrongSize) {
 			Assert::ExpectException<std::length_error>([]() {Tensor::zeroes({ 1 }).reshape({ 1, 2 }); });
 			Assert::ExpectException<std::length_error>([]() {Tensor::zeroes({ 3, 2 }).reshape({ 5, 1 }); });
 			Assert::ExpectException<std::length_error>([]() {Tensor::zeroes({ 7, 1, 3 }).reshape({ 5, 4, 1 }); });
 		}
 
-		TEST_METHOD(TestReshapes) {
+		TEST_METHOD(NewShape) {
 			Assert::AreEqual(1, Tensor::zeroes({}).reshape({ 1 }).getShape()[0]);
 
 			Assert::AreEqual(3, Tensor::zeroes({ 1, 3, 1 }).reshape({ 3 }).getShape()[0]);
@@ -83,27 +156,55 @@ namespace DeepLearningTest
 			Assert::AreEqual(7, Tensor::zeroes({ 5, 21 }).reshape({ 3,7,5 }).getShape()[1]);
 			Assert::AreEqual(5, Tensor::zeroes({ 5, 21 }).reshape({ 3,7,5 }).getShape()[2]);
 		}
+	};
 
-		TEST_METHOD(TestTooManyIndices) {
+	TEST_CLASS(GetTest)
+	{
+		TEST_METHOD(TooManyIndices) {
 			Assert::ExpectException<std::length_error>([]() {Tensor::zeroes({ }).get({ 0 }); });
 			Assert::ExpectException<std::length_error>([]() {Tensor::zeroes({ 2 }).get({ 1, 1 }); });
 			Assert::ExpectException<std::length_error>([]() {Tensor::zeroes({ 2, 4, 6 }).get({ 1, 1, 2, 2 }); });
 		}
 
-		TEST_METHOD(TestIndexOutOfBounds) {
+		TEST_METHOD(IndexOutOfBounds) {
 			Assert::ExpectException<std::out_of_range>([]() {Tensor::zeroes({ 1, 3 }).get({ 2, 1 }); });
 			Assert::ExpectException<std::out_of_range>([]() {Tensor::zeroes({ 3 }).get({ 4 }); });
 			Assert::ExpectException<std::out_of_range>([]() {Tensor::zeroes({ 4, 2, 8, 1, 2 }).get({ 3, 1, 5, 4 }); });
 		}
 
-		TEST_METHOD(TestGetterNewShape) {
+		TEST_METHOD(NewShape) {
 			Assert::AreEqual(5, Tensor::zeroes({ 1, 4, 5, 7 }).get({ 0, 0 }).getShape()[0]);
 			Assert::AreEqual(7, Tensor::zeroes({ 1, 4, 5, 7 }).get({ 0, 0 }).getShape()[1]);
 
 			Assert::AreEqual(2, Tensor::zeroes({ 3, 2 }).get({ 0 }).getShape()[0]);
 		}
 
-		TEST_METHOD(TestAssigningSingle) {
+		TEST_METHOD(MatchingValue) {
+			Tensor tensor1a = Tensor::full({ 2,3 }, 1.0f);
+			Tensor tensor1b = tensor1a.get({ 1 });
+			Assert::AreEqual(tensor1a.get({ 1,0 }).item(), tensor1b.get({ 0 }).item());
+
+			Tensor tensor2a = Tensor::zeroes({ 5,4,3,7 });
+			Tensor tensor2b = tensor2a.get({ 2, 3 });
+			Assert::AreEqual(tensor2a.get({ 2, 3, 1, 5 }).item(), tensor2b.get({ 1, 5 }).item());
+		}
+	};
+
+	TEST_CLASS(SetTest)
+	{
+		TEST_METHOD(TooManyIndices) {
+			Assert::ExpectException<std::length_error>([]() {Tensor::zeroes({ }).set({ 0 }, 0.0f); });
+			Assert::ExpectException<std::length_error>([]() {Tensor::zeroes({ 2 }).set({ 1, 1 }, 0.0f); });
+			Assert::ExpectException<std::length_error>([]() {Tensor::zeroes({ 2, 4, 6 }).set({ 1, 1, 2, 2 }, 0.0f); });
+		}
+
+		TEST_METHOD(IndexOutOfBounds) {
+			Assert::ExpectException<std::out_of_range>([]() {Tensor::zeroes({ 1, 3 }).set({ 2, 1 }, 0.0f); });
+			Assert::ExpectException<std::out_of_range>([]() {Tensor::zeroes({ 3 }).set({ 4 }, 0.0f); });
+			Assert::ExpectException<std::out_of_range>([]() {Tensor::zeroes({ 4, 2, 8, 1, 2 }).set({ 3, 1, 5, 4 }, 0.0f); });
+		}
+
+		TEST_METHOD(NewValue) {
 			Tensor tensor1 = Tensor::zeroes({ 3 });
 			tensor1.set(1.0f);
 			Assert::IsTrue(std::abs(tensor1.get({ 0 }).item() - 1.0f) < 1e-10);
